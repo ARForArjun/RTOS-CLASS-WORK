3b95759916c9089e11fc94b8a9fe089373c95260
Arjun P Sasi <133202043+ARForArjun@users.noreply.github.com>
2025-08-05 13:45:17 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,27 @@
-gg
+1. Write a program to take name and age as input and print a greeting. 
+
+name=input("enter your name:")
+age=int(input("enter your age:"))
+print(f"Hai, {name}, It's great to know you are {age} years old.")
+
+2. Write a program to print even numbers from 1 to 20.
+
+for number in range(1,21):
+  if number%2==0:
+    print(number)
+
+3. Write a function to return factorial of a number.
+
+  def factorial_iterative(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+i=int(input("enter the number:"))
+print(factorial_iterative(i))
+
+4. Store and display marks of 5 students.
+
+  
